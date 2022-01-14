e87355366d7775d72e56f3f6455e2dcb02021708
Xavi Ablaza <25378966+xaviablaza@users.noreply.github.com>
2022-01-14 11:51:30 +0800

    Add Redis::PooledClient

--- src/redis_mutex/lock.cr
+++ src/redis_mutex/lock.cr
@@ -4,7 +4,7 @@ require "./time_parser"
 class RedisMutex::Lock
   property key
 
-  def initialize(@key : String, @max_locking_time : Time::Span = RedisMutex::MAX_LOCKING_TIME, @redis = Redis.new)
+  def initialize(@key : String, @max_locking_time : Time::Span = RedisMutex::MAX_LOCKING_TIME, @redis : Redis::PooledClient | Redis = Redis.new)
   end
 
   def create_lock_threshold
